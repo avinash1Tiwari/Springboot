9c956aa3b9761f3258f9bab3289e5123dbc61d5d
avinash <avinashtiwari2405@gmail.com>
2024-08-14 00:44:28 +0530

    added basic setup

---
+++ springbootWeek1/src/main/java/springbootintroweek1/com/springbootWeek1/SpringbootWeek1Application.java
@@ -0,0 +1,13 @@
+package springbootintroweek1.com.springbootWeek1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeek1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeek1Application.class, args);
+	}
+
+}
---
+++ springbootWeek1/src/test/java/springbootintroweek1/com/springbootWeek1/SpringbootWeek1ApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootintroweek1.com.springbootWeek1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeek1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
